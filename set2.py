a437804ab5870d36effc9642e7f1a8dcfc59abd3
Adarsh tripathi <adarshtripathi9793085@gmail.com>
2026-01-02 23:48:02 +0530

    Add files via upload

---
+++ set2.py
@@ -0,0 +1,12 @@
+a = "ADarsh"
+b = "Tripathi"
+c = "20"
+d = "hello , have a nice day"
+print(a.upper())
+print(b.lower())
+print(a.capitalize())
+print(c.isnumeric())
+print(c.isalpha())
+print(d.startswith("hello"))
+print(d.replace("hello", "hi"))
+print(a.join(b))
